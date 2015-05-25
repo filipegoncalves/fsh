3325768cd8948c8d9ddb2bccee87a0c24e9334d8
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-25 15:47:54 +0100

    Implemented build_argv()

--- src/main.c
+++ src/main.c
@@ -5,21 +5,43 @@
 
 #define CMDS_DELIM ";"
 
-/*
-char **build_argv(char *cmd_line, size_t line_len) {
-	char **argv = malloc(line_len*sizeof(*argv));
+
+char **build_argv(char *cmd) {
+	char **argv = malloc((strlen(cmd)+1)*sizeof(*argv));
 
 	if (argv == NULL) {
 		return NULL;
 	}
 
-	size_t i;
-	
-}
-*/
+	size_t i = 0;
+	char *next_arg;
+	char *saveptr = NULL;
 
-void process_cmd(char *cmd_line) {
-	printf("Process command: %s\n", cmd_line);
+	next_arg = strtok_r(cmd, " ", &saveptr);
+	while (next_arg != NULL) {
+		argv[i++] = next_arg;
+		next_arg = strtok_r(NULL, " ", &saveptr);
+	}
+
+	argv[i] = NULL;
+
+	return argv;
+}
+
+
+void process_cmd(char *cmd) {
+	char **argv = build_argv(cmd);
+
+	if (argv == NULL) {
+		printf("Null argv\n");
+		return;
+	}
+
+	printf("Processed command: %s\n", cmd);
+	size_t i;
+	for (i = 0; argv[i] != NULL; i++) {
+		printf("argv[%zu] = %s\n", i, argv[i]);
+	}
 }
 
 void process_cmd_line(char *cmd_line) {
