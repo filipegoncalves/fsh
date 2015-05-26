f9f710b9450b85b88b4192d813bbef062dec2dcd
Filipe Gonçalves <filipe@codinghighway.com>
2015-05-26 15:21:39 +0100

    Implemented file I/O redirection

--- src/main.c
+++ src/main.c
@@ -7,7 +7,97 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define FILE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 #define CMDS_DELIM ";"
+
+enum redir_mode {
+	REDIR_APPEND,
+	REDIR_STDIN,
+	REDIR_STDOUT
+};
+
+void array_delete(char *arr[], size_t begin, size_t del_amount) {
+	size_t j = begin+del_amount;
+	while ((arr[begin++] = arr[j++]) != NULL);
+}
+
+int io_redir(enum redir_mode mode, char *filename) {
+	int flags;
+	int dup_target;
+	if (mode == REDIR_APPEND) {
+		flags = O_WRONLY | O_CREAT | O_APPEND;
+		dup_target = STDOUT_FILENO;
+	} else if (mode == REDIR_STDIN) {
+		flags = O_RDONLY;
+		dup_target = STDIN_FILENO;
+	} else if (mode == REDIR_STDOUT) {
+		flags = O_WRONLY | O_CREAT | O_TRUNC;
+		dup_target = STDOUT_FILENO;
+	} else {
+		fprintf(stderr, "Invalid redirection mode\n");
+		return -1;
+	}
+
+	int fd = open(filename, flags, FILE_MODE);
+	if (fd < 0) {
+		fprintf(stderr, "Couldn't open %s: ", filename);
+		perror(NULL);
+		return -1;
+	}
+
+	dup2(fd, dup_target);
+	close(fd);
+
+	return 0;
+}
+
+int do_redirect(char *argv[]) {
+	size_t i = 0;
+
+	while (argv[i] != NULL) {
+		if (*argv[i] != '<' && *argv[i] != '>') {
+			i++;
+			continue;
+		}
+
+		enum redir_mode mode;
+
+		if (*argv[i] == '>' && argv[i][1] == '>') {
+			mode = REDIR_APPEND;
+		} else if (*argv[i] == '>') {
+			mode = REDIR_STDOUT;
+		} else {
+			mode = REDIR_STDIN;
+		}
+
+		char *filename;
+		int to_shift;
+		if (mode != REDIR_APPEND) {
+			if (argv[i][1] != '\0') {
+				filename = &argv[i][1];
+				to_shift = 1;
+			} else {
+				filename = argv[i+1];
+				to_shift = 2;
+			}
+		} else {
+			if (argv[i][2] != '\0') {
+				filename = &argv[i][2];
+				to_shift = 1;
+			} else {
+				filename = argv[i+1];
+				to_shift = 2;
+			}
+		}
+
+		array_delete(argv, i, to_shift);
+		if (io_redir(mode, filename) < 0) {
+			return -1;
+		}
+	}
+
+	return 0;
+}
 
 int pipe_exec(char *argv[]) {
 	int pipefd[2];
@@ -28,6 +118,11 @@ int pipe_exec(char *argv[]) {
 	if (f == 0) {
 		dup2(pipefd[1], STDOUT_FILENO);
 		close(pipefd[0]);
+
+		if (do_redirect(argv) < 0) {
+			exit(EXIT_FAILURE);
+		}
+
 		if (execvp(argv[0], argv) < 0) {
 			perror("execvp() failed");
 			exit(EXIT_FAILURE);
@@ -63,6 +158,10 @@ void execute_cmd(char *argv[]) {
 	}
 
 	if (f == 0) {
+		if (do_redirect(&argv[j]) < 0) {
+			exit(EXIT_FAILURE);
+		}
+
 		if (execvp(argv[j], &argv[j]) < 0) {
 			// TODO Enhance error reporting
 			perror("execvp() failed");
